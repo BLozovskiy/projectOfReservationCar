21e2afe8f744570de43182744ac21dfc0524794f
BLozovskiy <138723832+BLozovskiy@users.noreply.github.com>
2023-08-02 12:55:01 +0300

    Update main.c
    Створив структуру та додав бібліотеки

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct {
+    int id;
+    char model[100];
+    char brand[20];
+    char class[100];
+    char body[100];
+    char trasmissin[100];
+    char fuel[100];
+    char engine[100];
+    float price, engineCapacity, enginePower;
+} Car;
 
 int main()
 {
